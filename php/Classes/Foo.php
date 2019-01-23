e48b3812158efa7dcaf2ed88ac0fdb2586bb7d61
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-23 14:49:42 -0700

    Continued adding state variables.

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -16,6 +16,26 @@ class Author {
 	 **/
 	private $authorID;
 	/**
-	 *
+	 * avatar URL for this Author
+	 * @var string $authorAvatarUrl
 	 **/
+	private $authorAvatarUrl;
+	/**
+	 * activation token for Author
+	 * @var string $authorActivationtoken
+	 **/
+	private $authorActivationToken;
+	/**
+	 * e-mail address for this Author; this a unique key
+	 * @var string $authorEmail
+	 **/
+	private $authorEmail;
+	/**
+	 * password for this Author
+	 * @var string $authorHash
+	 **/
+	private $authorHash;
+
 }
+
+
