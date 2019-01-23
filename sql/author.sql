9bbca194aa75b285585c5a970bc019825fa4b2f6
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-23 13:20:14 -0700

    Created an author table.

---
+++ sql/author.sql
@@ -0,0 +1,12 @@
+CREATE TABLE author(
+	authorId BINARY(16) NOT NULL,
+	authorAvatarUrl VARCHAR(255),
+	authorActivationToken CHAR(32),
+	authorEmail VARCHAR(128) NOT NULL,
+	authorHash CHAR(97) NOT NULL,
+	authorUsername VARCHAR(32) NOT NULL,
+	UNIQUE(authorEmail),
+	UNIQUE(authorUsername),
+	INDEX(authorEmail),
+	PRIMARY KEY(authorId)
+	);
