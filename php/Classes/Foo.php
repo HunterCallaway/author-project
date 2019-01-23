8c72f2957c6d8c531c63b034de6ab1a4c51ee321
Hunter Callaway <HunterCallaway@hotmail.com>
2019-01-23 14:26:05 -0700

    Started coding the Author class in Foo.php and created the autoload.php file.

---
+++ php/Classes/Foo.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* Cross section of a profile
+*
+* This is a cross section of a profile referred to as author. This entity is a strong entity.
+*
+* @author Hunter Callaway <jcallaway3@cnm.edu>
+**/
+
+class Author {
+
+	/**
+	 * id for the Author entity; this is the Primary Key
+	 * @var Uuid $authorID
+	 **/
+	private $authorID;
+	/**
+	 *
+	 **/
+}
